01255176b0fc4937339389ce59361ac984185e3f
Guilhem Sante <guilhem.snt@gmail.com>
2025-02-03 21:00:42 +0100

    refactor: internal asynchronous flow

--- gymnasium_gomokurs/domain/gymnasium_gomokurs/service.py
+++ gymnasium_gomokurs/domain/gymnasium_gomokurs/service.py
@@ -1,58 +1,45 @@
 from typing import Optional
 import numpy as np
-import gymnasium as gym
+import gymnasium
+from .ports import ManagerInterface
+from .models.game import *
+import asyncio
 
-class GomokursEnv(gym.Env):
-    def __init__(self, size: int = 5):
-        # The size of the square grid
+class GomokursEnv(gymnasium.Env):
+    def __init__(self, manager_interface: ManagerInterface, size: int = 20):
         self.size = size
-
-        # Define the agent and target location; randomly chosen in `reset` and updated in `step`
-        self._agent_location = np.array([-1, -1], dtype=np.int32)
-        self._target_location = np.array([-1, -1], dtype=np.int32)
-
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`-1}^2
-        self.observation_space = gym.spaces.Dict(
+        self._manager_interface = manager_interface
+        self._board = np.zeros((size, size))
+        self.observation_space = gymnasium.spaces.Dict(
             {
-                "agent": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": gym.spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "availables":   gymnasium.spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "player":       gymnasium.spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "opponent":     gymnasium.spaces.Box(0, size - 1, shape=(2,), dtype=int),
             }
-        )
+        ) 
+        self.action_space = gymnasium.spaces.Discrete(size ** 2)
+        self.loop = asyncio.get_event_loop()
 
-        # We have 4 actions, corresponding to "right", "up", "left", "down"
-        self.action_space = gym.spaces.Discrete(4)
-        # Dictionary maps the abstract actions to the directions on the grid
-        self._action_to_direction = {
-            0: np.array([1, 0]),  # right
-            1: np.array([0, 1]),  # up
-            2: np.array([-1, 0]),  # left
-            3: np.array([0, -1]),  # down
-        }
+    def _action_to_move(self, action_idx: int) -> Move:
+        x = action_idx / self.size
+        y = action_idx % self.size
+
+        return Move(x, y)
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
-
-    def _get_info(self):
         return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
+            "availables":   (self._board == CellStatus.AVAILABLE).astype(int),
+            "player":       (self._board == CellStatus.PLAYER).astype(int),
+            "opponent":     (self._board == CellStatus.OPPONENT).astype(int),
         }
 
+    def _get_info(self):
+        return {}
+
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
-        # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(
-                0, self.size, size=2, dtype=int
-            )
+        self.board = np.zeros((self.size, self.size))
 
         observation = self._get_obs()
         info = self._get_info()
@@ -60,18 +47,22 @@ class GomokursEnv(gym.Env):
         return observation, info
 
     def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid bounds
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
+        move = self._action_to_move(action)
+        if self._board[move.x][move.y] != CellStatus.AVAILABLE:
+            raise Exception(f"player move invalid: cell at position {move} is not available")
+        self._board[move.x][move.y] = CellStatus.PLAYER
 
-        # An environment is completed if and only if the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
+        self._manager_interface.notify_move(move)
+
+        opponent_move = self._manager_interface.get_opponent_turn()
+        if self._board[opponent_move.x][opponent_move.y] != CellStatus.AVAILABLE:
+            raise Exception(f"opponent move invalid: cell at position {opponent_move} is not available")
+        self._board[move.x][move.y] = CellStatus.OPPONENT
+
+        terminated = False
         truncated = False
-        reward = 1 if terminated else 0  # the agent is only reached at the end of the episode
+        reward = 0
         observation = self._get_obs()
         info = self._get_info()
 
-        return observation, reward, terminated, truncated, info
+        return terminated, truncated, reward, observation, info
