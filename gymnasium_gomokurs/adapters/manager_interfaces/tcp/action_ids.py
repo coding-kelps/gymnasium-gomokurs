afd1d834f119a04e4db545ae471f1daa195aa7ce
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2025-02-04 17:49:30 +0100

    feat: remove asynchronous flow from tcp manager interface

--- gymnasium_gomokurs/adapters/manager_interfaces/tcp/action_ids.py
+++ gymnasium_gomokurs/adapters/manager_interfaces/tcp/action_ids.py
@@ -3,25 +3,25 @@ from enum import Enum
 class ActionID(Enum):
     # Action that can be send from the manager to the player.
 
-    MANAGER_PROTOCOL_COMPATIBLE = 0x00
-    MANAGER_START               = 0x01
-    MANAGER_TURN                = 0x02
-    MANAGER_BEGIN               = 0x03
-    MANAGER_BOARD               = 0x04
-    MANAGER_INFO                = 0x05
-    MANAGER_END                 = 0x06
-    MANAGER_ABOUT               = 0x07
-    MANAGER_UNKNOWN             = 0x08
-    MANAGER_ERROR               = 0x09
+    MANAGER_PROTOCOL_COMPATIBLE = 0x00.to_bytes(1, 'big')
+    MANAGER_START               = 0x01.to_bytes(1, 'big')
+    MANAGER_TURN                = 0x02.to_bytes(1, 'big')
+    MANAGER_BEGIN               = 0x03.to_bytes(1, 'big')
+    MANAGER_BOARD               = 0x04.to_bytes(1, 'big')
+    MANAGER_INFO                = 0x05.to_bytes(1, 'big')
+    MANAGER_END                 = 0x06.to_bytes(1, 'big')
+    MANAGER_ABOUT               = 0x07.to_bytes(1, 'big')
+    MANAGER_UNKNOWN             = 0x08.to_bytes(1, 'big')
+    MANAGER_ERROR               = 0x09.to_bytes(1, 'big')
 
     # Actions that can be send from the player to the manager.
 
-    PLAYER_PROTOCOL_VERSION     = 0x0A
-    PLAYER_READY                = 0x0B
-    PLAYER_PLAY                 = 0x0C
-    PLAYER_METADATA             = 0x0D
-    PLAYER_UNKNOWN              = 0x0E
-    PLAYER_ERROR                = 0x0F
-    PLAYER_MESSAGE              = 0x10
-    PLAYER_DEBUG                = 0x11
-    PLAYER_SUGGESTION           = 0x12
+    PLAYER_PROTOCOL_VERSION     = 0x0A.to_bytes(1, 'big')
+    PLAYER_READY                = 0x0B.to_bytes(1, 'big')
+    PLAYER_PLAY                 = 0x0C.to_bytes(1, 'big')
+    PLAYER_METADATA             = 0x0D.to_bytes(1, 'big')
+    PLAYER_UNKNOWN              = 0x0E.to_bytes(1, 'big')
+    PLAYER_ERROR                = 0x0F.to_bytes(1, 'big')
+    PLAYER_MESSAGE              = 0x10.to_bytes(1, 'big')
+    PLAYER_DEBUG                = 0x11.to_bytes(1, 'big')
+    PLAYER_SUGGESTION           = 0x12.to_bytes(1, 'big')
--- gymnasium_gomokurs/adapters/manager_interfaces/tcp/manager_interface.py
+++ gymnasium_gomokurs/adapters/manager_interfaces/tcp/manager_interface.py
@@ -1,255 +1,265 @@
-import asyncio
 from .action_ids import ActionID
 from ....domain.gymnasium_gomokurs.ports import ManagerInterface
 from ....domain.gymnasium_gomokurs.models.game import *
 from typing import Dict
 import numpy as np
 import logging
+import socket
 
 logger = logging.getLogger(__name__)
 
-class TCPManagerInterface(ManagerInterface):
+def create_tcp_manager_interface_from_active_connection(host: str = "localhost", port: int = 49912):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    tcp = TcpManagerInterface(s)
+
+    return tcp
+    
+def create_tcp_manager_interface_from_passive_connection(host: str = "localhost", port: int = 49912):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen()
+
+    conn, addr = s.accept()
+    logging.debug(f"accepted tcp connection with manager of address: {addr}")
+    tcp = TcpManagerInterface(conn)
+
+    return tcp
+
+class TcpManagerInterface(ManagerInterface):
     PROTOCOL_VERSION = "0.1.0"
 
-    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        self.reader = reader
-        self.writer = writer
-        self.loop = asyncio.get_event_loop()
+    def __init__(self, conn: socket.socket):
+        self.conn = conn
 
         try:
-            self.loop.run_until_complete(self._check_protocol_compatibility())
+            self._check_protocol_compatibility()
         except Exception as e:
             raise Exception(f"protocol compability check failed: {e}")
     
     def get_init_state(self) -> tuple[int, np.ndarray]:
-        return self.loop.run_until_complete(self._receive_init_state())
+        return self._receive_init_state()
     
     def get_opponent_turn(self) -> tuple[Move | None, bool | None]:
-        return self.loop.run_until_complete(self._receive_opponent_turn())
+        return self._receive_opponent_turn()
     
     def notify_move(self, move: Move) -> None:
-        return self.loop.run_until_complete(self._send_move(move))
+        return self._send_move(move)
     
     def notify_error(self, error_msg: str) -> None:
-        return self.loop.run_until_complete(self._send_error(error_msg))
+        return self._send_error(error_msg)
 
-    async def _check_protocol_compatibility(self):
-        data = bytearray(ActionID.PLAYER_PROTOCOL_VERSION)
+    def _check_protocol_compatibility(self):
+        self.conn.sendall(ActionID.PLAYER_PROTOCOL_VERSION.value)
 
         encoded_version = self.PROTOCOL_VERSION.encode("utf-8")
 
-        data.append(len(encoded_version).to_bytes(4, 'big'))
-        data.append(encoded_version)
+        self.conn.sendall(len(encoded_version).to_bytes(4, 'big'))
+        self.conn.sendall(encoded_version)
 
-        self.writer.write(data)
-        await self.writer.drain()
-
-        data = await self.reader.read(1)
-
+        data = self.conn.recv(1)
         if not data:
             raise Exception("connection to manager closed before protocol validation")
-        elif data[0] == ActionID.MANAGER_PROTOCOL_COMPATIBLE:
+        
+        if data == ActionID.MANAGER_PROTOCOL_COMPATIBLE.value:
             logging.debug("manager recognized protocol as compatible")
             return
-        elif data[0] == ActionID.MANAGER_UNKNOWN:
+        elif data == ActionID.MANAGER_UNKNOWN.value:
             raise Exception("manager does not know protocol compatibility check action")
-        elif data[0] == ActionID.MANAGER_ERROR:
-            data = self.socket.recv(4)
+        elif data == ActionID.MANAGER_ERROR.value:
+            data = self.conn.recv(4)
             payload_len = int.from_bytes(data, 'big')
 
-            data = self.socket.recv(payload_len)
+            data = self.conn.recv(payload_len)
             error_msg = data.decode("utf-8")
 
             raise Exception(f"manager error: {error_msg}")
         else:
-            raise Exception(f"unexpected manager action with ID {data[0]} at protocol compatibility validation")
+            raise Exception(f"unexpected manager action with ID {data} at protocol compatibility validation")
     
-    async def _receive_init_state(self) -> tuple[int, np.ndarray]:
+    def _receive_init_state(self) -> tuple[int, np.ndarray]:
+        board_initialized = False
+
         while True:        
-            data = await self.reader.read(1)
+            data = self.conn.recv(1)
             if not data:
                 raise Exception("connection closed by the remote host")
             
-            if data[0] == ActionID.MANAGER_START:
-                size = self.loop.run_until_complete(self._start_handler())
+            if data == ActionID.MANAGER_START.value:
+                size = self._start_handler()
                 board = np.zeros((size, size))
+                board_initialized = True
 
                 logging.debug(f"initialized board following START action")
 
-                await self._send_readiness()
-            elif data[0] == ActionID.MANAGER_TURN:
-                move = self.loop.run_until_complete(self._turn_handler())
+                self._send_readiness()
+            elif data == ActionID.MANAGER_TURN.value:
+                move = self._turn_handler()
 
-                if not board:
+                if not board_initialized:
                     err = "unexpected TURN action before game initialization"
-                    await self._send_error(err)
+                    self._send_error(err)
                     raise Exception(err)
                     
-                board[move.x][move.y] = CellStatus.OPPONENT
+                board[move.x][move.y] = CellStatus.OPPONENT.value
 
                 logging.debug(f"initialized state following TURN action")
                 return (size, board)
-            elif data[0] == ActionID.MANAGER_BEGIN:
-                if not board:
+            elif data == ActionID.MANAGER_BEGIN.value:
+                if not board_initialized:
                     err = "unexpected BEGIN action before game initialization"
-                    await self._send_error(err)
+                    self._send_error(err)
                     raise Exception(err)
                 
                 logging.debug(f"initialized state following BEGIN action")
                 return (size, board)
-            elif data[0] == ActionID.MANAGER_BOARD:
-                if not board:
+            elif data == ActionID.MANAGER_BOARD.value:
+                if not board_initialized:
                     err = "unexpected BOARD action before game initialization"
-                    await self._send_error(err)
+                    self._send_error(err)
                     raise Exception(err)
 
-                turns = await self._board_handler()
+                turns = self._board_handler()
 
                 for turn in turns:
-                    board[turn.move.x][turn.move.y] = CellStatus.PLAYER if turn.field == RelativeField.OWN_STONE else CellStatus.OPPONENT
+                    board[turn.move.x][turn.move.y] = CellStatus.PLAYER.value if turn.field == RelativeField.OWN_STONE.value else CellStatus.OPPONENT.value
                 
                 logging.debug(f"initialized state following BOARD action")
                 return (size, board)
             else:
-                err = f"unexpected action with id {data[0]} before game initialization"
-                await self._send_error(err)
+                err = f"unexpected action with id {data} before game initialization"
+                self._send_error(err)
                 raise Exception(err)
     
-    async def _receive_opponent_turn(self) -> tuple[Move | None, bool | None]:
+    def _receive_opponent_turn(self) -> tuple[Move | None, bool | None]:
         while True:        
-            data = await self.reader.read(1)
+            data = self.conn.recv(1)
             if not data:
                 raise Exception("connection closed by the remote host")
             
-            if data[0] == ActionID.MANAGER_TURN:
-                move = await self._turn_handler()
+            if data == ActionID.MANAGER_TURN.value:
+                move = self._turn_handler()
 
                 return (move, None)
-            elif data[0] == ActionID.MANAGER_END:
+            elif data == ActionID.MANAGER_END.value:
                 return None, True
-            elif data[0] == ActionID.MANAGER_INFO:
-                info = await self._info_handler()
+            elif data == ActionID.MANAGER_INFO.value:
+                info = self._info_handler()
 
                 logging.info(f"received info: {info}")
                 continue
-            elif data[0] == ActionID.MANAGER_UNKNOWN:
-                unknown = await self._unknown_handler()
+            elif data == ActionID.MANAGER_UNKNOWN.value:
+                unknown = self._unknown_handler()
                 raise Exception(f"manager error: {unknown}")
-            elif data[0] == ActionID.MANAGER_ERROR:
-                error = await self._error_handler()
+            elif data == ActionID.MANAGER_ERROR.value:
+                error = self._error_handler()
                 raise Exception(f"manager error: {error}")
-            elif data[0] == ActionID.MANAGER_ABOUT:
-                await self._about_handler()
+            elif data == ActionID.MANAGER_ABOUT.value:
+                self._about_handler()
 
                 logging.debug(f"send metadata following ABOUT action")
                 continue
             else:
-                err = f"unexpected action with id {data[0]} after game initialization"
-                await self._send_error(err)
+                err = f"unexpected action with id {data} after game initialization"
+                self._send_error(err)
                 raise Exception(err)
 
-    async def _start_handler(self) -> int:
-        data = await self.reader.read(1)
+    def _start_handler(self) -> int:
+        data = self.conn.recv(1)
 
         size = int.from_bytes(data, 'big')
 
         return size
 
-    async def _turn_handler(self) -> Move:
-        data = await self.reader.read(2)
+    def _turn_handler(self) -> Move:
+        data = self.conn.recv(2)
 
-        move = Move(int.from_bytes(data[0], 'big'), int.from_bytes(data[1], 'big'))
+        move = Move(int.from_bytes(data[:1], 'big'), int.from_bytes(data[1:], 'big'))
 
-        await self.opponent_turn_queue.put(move)
+        return move
 
-    async def _board_handler(self)-> list[RelativeTurn]:
+    def _board_handler(self)-> list[RelativeTurn]:
         TURN_PACKET_SIZE = 3
 
-        data = await self.reader.read(4)
+        data = self.conn.recv(4)
 
         nb_turn = int.from_bytes(data, 'big')
 
-        data = await self.reader.read(nb_turn * TURN_PACKET_SIZE)
+        data = self.conn.recv(nb_turn * TURN_PACKET_SIZE)
 
-        turns = [(x, y, field) for x, y, field in zip(data[0::3], data[1::3], data[2::3])]
+        turns = []
+        for i in range(nb_turn, step = 3):
+            x       = int.from_bytes(data[i:i+1],   'big')
+            y       = int.from_bytes(data[i+1:i+2], 'big')
+            field   = int.from_bytes(data[i+2:i+3], 'big')
+
+            turns.append((x, y, field))
 
         return turns
 
-    async def _info_handler(self)-> str:
-        data = self.socket.recv(4)
+    def _info_handler(self)-> str:
+        data = self.conn.recv(4)
         payload_len = int.from_bytes(data, 'big')
 
-        data = self.socket.recv(payload_len)
+        data = self.conn.recv(payload_len)
         info = data.decode("utf-8")
 
         return info
     
-    async def _about_handler(self)-> None:
-        await self._send_metadata({"name": "gymnasium-gomokurs"})
+    def _about_handler(self)-> None:
+        self._send_metadata({"name": "gymnasium-gomokurs"})
     
-    async def _unknown_handler(self)-> str:
-        data = self.socket.recv(4)
+    def _unknown_handler(self)-> str:
+        data = self.conn.recv(4)
         payload_len = int.from_bytes(data, 'big')
 
-        data = self.socket.recv(payload_len)
+        data = self.conn.recv(payload_len)
         unknown_msg = data.decode("utf-8")
 
         return unknown_msg
 
-    async def _error_handler(self)-> str:
-        data = self.socket.recv(4)
+    def _error_handler(self)-> str:
+        data = self.conn.recv(4)
         payload_len = int.from_bytes(data, 'big')
 
-        data = self.socket.recv(payload_len)
+        data = self.conn.recv(payload_len)
         error_msg = data.decode("utf-8")
 
         return error_msg
 
-    async def _send_readiness(self) -> None:
-        data = bytearray(ActionID.PLAYER_READY)
+    def _send_readiness(self) -> None:
+        self.conn.sendall(ActionID.PLAYER_READY.value)
 
-        self.writer.write(data)
-        await self.writer.drain()
 
-    async def _send_move(self, move) -> None:
-        data = bytearray(ActionID.PLAYER_PLAY)
+    def _send_move(self, move) -> None:
+        self.conn.sendall(ActionID.PLAYER_PLAY.value)
 
-        data.append(move.x.to_bytes(1, 'big'))
-        data.append(move.y.to_bytes(1, 'big'))
+        self.conn.sendall(move.x.to_bytes(1, 'big'))
+        self.conn.sendall(move.y.to_bytes(1, 'big'))
 
-        self.writer.write(data)
-        await self.writer.drain()
 
-    async def _send_metadata(self, metadata: Dict[str, str]):
-        data = bytearray(ActionID.PLAYER_METADATA)
+    def _send_metadata(self, metadata: Dict[str, str]):
+        self.conn.sendall(ActionID.PLAYER_METADATA.value)
 
         fmt_metadata = ",".join([f"{k}={v}" for k, v in metadata.items()])
         encoded_fmt_metadata = fmt_metadata.encode("utf-8")
 
-        data.append(len(encoded_fmt_metadata).to_bytes(4, 'big'))
-        data.append(encoded_fmt_metadata)
+        self.conn.sendall(len(encoded_fmt_metadata).to_bytes(4, 'big'))
+        self.conn.sendall(encoded_fmt_metadata)
 
-        self.writer.write(data)
-        await self.writer.drain()
-
-    async def _send_unknown(self, msg: str) -> None:
-        data = bytearray(ActionID.PLAYER_UNKNOWN)
+    def _send_unknown(self, msg: str) -> None:
+        self.conn.sendall(ActionID.PLAYER_UNKNOWN.value)
 
         encoded_msg = msg.encode("utf-8")
 
-        data.append(len(encoded_msg).to_bytes(4, 'big'))
-        data.append(encoded_msg)
+        self.conn.sendall(len(encoded_msg).to_bytes(4, 'big'))
+        self.conn.sendall(encoded_msg)
 
-        self.writer.write(data)
-        await self.writer.drain()
-
-    async def _send_error(self, msg: str) -> None:
-        data = bytearray(ActionID.PLAYER_MESSAGE)
+    def _send_error(self, msg: str) -> None:
+        self.conn.sendall(ActionID.PLAYER_MESSAGE.value)
 
         encoded_msg = msg.encode("utf-8")
 
-        data.append(len(encoded_msg).to_bytes(4, 'big'))
-        data.append(encoded_msg)
-
-        self.writer.write(data)
-        await self.writer.drain()
+        self.conn.sendall(len(encoded_msg).to_bytes(4, 'big'))
+        self.conn.sendall(encoded_msg)
