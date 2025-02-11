d3037991fd58c37f0c47981d233b8808a945037d
Guilhem Santé <guilhem.snt@gmail.com>
2025-02-11 17:09:41 +0100

    feat: set global logger for gymnasium-gomokurs

--- examples/main.py
+++ examples/main.py
@@ -2,6 +2,8 @@ from gymnasium_gomokurs.domain.gymnasium_gomokurs.service import GomokursEnv
 from gymnasium_gomokurs.adapters.manager_interfaces.tcp.manager_interface import *
 import numpy as np
 
+logging.getLogger("gymnasium-gomokurs").setLevel(logging.DEBUG)
+
 def main():
     tcp_interface_manager = create_tcp_manager_interface_from_active_connection()
 
